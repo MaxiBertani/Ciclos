e0a18f7253f113a6521d24c8b774195155fddccf
MaxiBertani <mbertani@frsn.utn.edu.ar>
2024-04-18 16:00:27 -0300

    clase de ciclos

---
+++ ciclos.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main (){
+
+int ValorIngresado=10;
+
+for (int iterator = 0; iterator <=ValorIngresado ; iterator++){
+
+
+if (iterator % 2 == 0){
+
+
+cout << iterator<< endl;
+
+
+
+
+
+}
+
+}
+    return 0;
+}
